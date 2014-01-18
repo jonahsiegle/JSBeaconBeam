29219452106bc6af890b9e40e556a5c15248ef45
jonahsiegle <jonah@jonahsiegle.com>
2014-01-17 22:51:50 -0700

    Initial Commit

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSBeaconBeam
+//
+//  Created by Jonah Siegle on 1/17/2014.
+//  Copyright (c) 2014 Jonah Siegle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSBeaconBeam
+//
+//  Created by Jonah Siegle on 1/17/2014.
+//  Copyright (c) 2014 Jonah Siegle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
